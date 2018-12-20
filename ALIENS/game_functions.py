b75117ac0445d4ebd392dbf848b430d2ab5d9e05
Lihuan_Li <z5139949@student.unsw.edu.au>
2018-12-20 22:27:37 +0800

    level.

--- ALIENS/game_functions.py
+++ ALIENS/game_functions.py
@@ -27,7 +27,7 @@ def check_keyup_events(ship, event):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
+def check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets):
     # react to mouse click and keyboard.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -42,25 +42,28 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
             check_keyup_events(ship, event)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
-def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
-        # reset all game settings.s
+        # reset all game settings.
         ai_settings.initialize_dynamic_settings()
         # hide the mouse.
         pygame.mouse.set_visible(False)
-    # start the game when clicking "play".
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        # reset stats info.
+        stats.reset_stats()
         stats.game_active = True
-
-    # make aliens and bullets empty.
-    aliens.empty()
-    bullets.empty()
-    # make new aliens and let the ship in the center.
-    create_fleet(ai_settings, screen, ship, aliens)
-    ship.center_ship()
+        # reset scoreboard image.
+        sb.prep_score()
+        sb.prep_high_score() 
+        sb.prep_level()
+        # make aliens and bullets empty.
+        aliens.empty()
+        bullets.empty()
+        # make new aliens and let the ship in the center.
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
 def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
 
@@ -104,11 +107,15 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, 
             stats.score += ai_settings.alien_points * len(aliens)
             sb.prep_score()
         check_high_score(stats, sb)
-        
+
     # if there are no aliens.
     if len(aliens) == 0:
         bullets.empty()
         ai_settings.increase_speed()
+        # then level up.
+        stats.level += 1
+        sb.prep_level()
+
         create_fleet(ai_settings, screen, ship, aliens)
 
 def get_number_aliens_x(ai_settings, alien_width):
--- ALIENS/scoreboard.py
+++ ALIENS/scoreboard.py
@@ -14,6 +14,7 @@ class Scoreboard():
         # image of score.
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):     
         # change score into image.
@@ -30,6 +31,7 @@ class Scoreboard():
         # show score and highest score on the screen.
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
 
     def prep_high_score(self):
         # change highest score into image.
@@ -40,3 +42,10 @@ class Scoreboard():
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx 
         self.high_score_rect.top = self.score_rect.top
+
+    def prep_level(self):
+        self.level_image = self.font.render(str(self.stats.level), True, self.text_color, self.ai_settings.bg_color)
+
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right 
+        self.level_rect.top = self.score_rect.bottom + 10
